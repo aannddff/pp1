21dee59fc9a8a0dc062d80547a260011df4f5848
Andrey <102661790+aannddff@users.noreply.github.com>
2025-04-03 14:52:33 +0300

    Update Main.java
    изменение main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,30 +1,24 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserService userService = new UserServiceImpl();
 
-        userDao.createUsersTable();
+        userService.createUsersTable();
 
-//        userDao.saveUser("Ivan", "A", (byte) 20);
-//        userDao.saveUser("Ann", "B", (byte) 22);
-//        userDao.saveUser("Sem", "C", (byte) 24);
-//        userDao.saveUser("Artem", "D", (byte) 25);
 
-        List<User> users = userDao.getAllUsers();
+        List<User> users = userService.getAllUsers();
         for (User user : users) {
             System.out.println(user);
         }
 
-        userDao.cleanUsersTable();
-
-        userDao.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
